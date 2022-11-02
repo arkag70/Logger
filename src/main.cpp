6c8a062615c95105ed44c0b206fcd8cbe4e50219
Arkaprabha Ghosh <arkag70@gmail.com>
2022-11-02 13:37:55 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include "logger.hpp"
+#include <thread>
+
+void calculate(int a, int b){
+
+    int add = a+b;
+    LOG_FATAL("Sum : %d", add);
+
+    int sub = a-b;
+    LOG_FATAL("Diff : %d", add);
+
+    int pro = a*b;
+    LOG_FATAL("Prod : %d", pro);
+
+}
 
 int main(){
 
     const char *name = "Arka";
     int age{26};
     const char *place = "Kolkata";
-    Logger::Fatal("Hello my name is %s", name);
-    Logger::Fatal("I am %d years old", age);
-    Logger::Fatal("I stay in %s", place);
-    // Logger::Error("Hello my name is :%s", name);
-    // Logger::Warning("Hello my name is :%s", name);
-    // Logger::Info("Hello my name is :%s", name);
-    // Logger::Debug("Hello my name is :%s", name);
-    // Logger::Trace("Size of enum :%d", sizeof(LogChannel));
+
+    std::thread calc(calculate, 20, 15);
+
+    calculate(10, 5);
+
+
+    LOG_FATAL("Hello my name is %s", name);
+    LOG_FATAL("I am %d years old", age);
+    LOG_FATAL("I stay in %s", place);
+
+    calc.join();
+
+
 
 
     return 0;
